7e8fbeaec1373268fffd9d6a16a412304ba61a0c
Felix Fietkau <nbd@nbd.name>
2022-06-23 10:46:19 +0200

    example: create wireguard and tunnel device
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- examples/test-net0.sh
+++ examples/test-net0.sh
@@ -2,10 +2,15 @@
 ifname="${1:-wg0}"
 host="${2:-ap1}"
 
+ip link add dev $ifname type wireguard > /dev/null 2>&1
+
 ../unetd -d -h $PWD/hosts -N '{
 	"name": "'"$ifname"'",
 	"type": "file",
 	"key": "'"$(cat ./net0-${host}.key)"'",
 	"file": "'"$PWD/net0.json"'",
+	"tunnels": {
+		"vx0": "l2-tunnel"
+	},
 	"update-cmd": "'"$PWD/../scripts/update-cmd.pl"'"
 }'
