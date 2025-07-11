b06199e8f9e7dfd22e245ca645e0e763902462a2
Felix Fietkau <nbd@nbd.name>
2025-07-11 11:31:40 +0200

    sntrup761: add function for deriving public key from secret key
    Signed-off-by: Felix Fietkau <nbd@nbd.name>

--- sntrup761.h
+++ sntrup761.h
@@ -10,6 +10,7 @@
 
 void sntrup761_set_batch(int val);
 int sntrup761_keypair(uint8_t *pk, uint8_t *sk);
+int sntrup761_pubkey(uint8_t *pk, const uint8_t *sk);
 int sntrup761_enc(uint8_t *c, uint8_t *k, const uint8_t *pk);
 int sntrup761_dec(uint8_t *k, const uint8_t *c, const uint8_t *sk);
 
